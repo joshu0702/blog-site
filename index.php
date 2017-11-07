58d9dc961e72ad711e54c1f61a6b0834aa1b8fec
aviavinas <32093890+aviavinas@users.noreply.github.com>
2017-11-07 10:24:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>BlogIndi.Com | Write your Blog</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<link rel="stylesheet" href="gallery/css/bootstrap.min.css" />
+	<style>
+		body, html {
+			height: 100%;
+		}
+		body {
+			background-color: #f5f8fa;
+		}
+		.navbar-brand {
+			color: #e83e8c !important;
+		}
+		.ly {
+			min-height: 100px;
+			background-color: #fff;
+		}
+		.ly ul li {
+			border-bottom: 1px solid #fafafa;
+			padding: 10px;
+			font-weight: 600;
+		}
+		.card-body {
+			box-shadow: 0px 0px 16px 0px #607d8b61;
+		}
+		.lt-post, .rt-post {
+			border-left: 8px solid #f5f8fa;
+			padding-top: .5rem!important;
+		}
+		.brand-tit {
+			color: white;
+	    margin-left: 10px;
+		}
+		.navbar-brand {
+			border-right: 2px solid #2d3238;
+		}
+	</style>
+	</head>
+<body>
+	<nav id="myNav" class="navbar navbar-dark bg-dark fixed-top navbar-expand-sm">
+		<img src="gallery/img/logo.png" class="navbar-brand mr-0" width="60px" alt="Logo" />
+		<h4 class="brand-tit">Lpu Post</h4>
+
+		<div class="col-lg-6 pl-4">
+	    <div class="input-group">
+	      <input type="text" class="form-control" placeholder="Search for..." aria-label="Search for...">
+	      <span class="input-group-btn">
+	        <button class="btn btn-secondary" type="button">Search</button>
+	      </span>
+	    </div>
+	  </div>
+
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBarMain">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-end" id="navBarMain">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a href="#contact" class="nav-link m-1">Top Story</a>
+				</li>
+				<li class="nav-item">
+					<a href="#contact" class="nav-link m-1">Trending</a>
+				</li>
+				<li class="nav-item">
+					<a href="#contact" class="nav-link m-1">Contact</a>
+				</li>
+				<li class="nav-item">
+					<a href="#about" class="nav-link m-1">About</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<div id="carouseSlideshow" class="carousel slide mb-2" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouseSlideshow" data-slide-to="0" class="active"></li>
+			<li data-target="#carouseSlideshow" data-slide-to="1"></li>
+	    <li data-target="#carouseSlideshow" data-slide-to="2"></li>
+			<li data-target="#carouseSlideshow" data-slide-to="3"></li>
+	    <li data-target="#carouseSlideshow" data-slide-to="4"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100" src="gallery/img/s1.png" alt="First slide">
+				<div class="carousel-caption d-none d-md-block">
+			    <h3>The Pine Trees that Point to the Equator</h3>
+			    <p>If you look closely, no matter where you see them, they're all leaning toward the equator. All of them.</p>
+			  </div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="gallery/img/s2.png" alt="Second slide">
+				<div class="carousel-caption d-none d-md-block">
+			    <h3>Do Humans Have An Internal Compass?</h3>
+			    <p>Birds, whales, butterflies, and many other animals can sense magnetic fields, can we?</p>
+			  </div>
+	    </div>
+			<div class="carousel-item">
+	      <img class="d-block w-100" src="gallery/img/s3.png" alt="Third slide">
+				<div class="carousel-caption d-none d-md-block">
+			    <h3>Can You Really Mistake Thirst For Hunger?</h3>
+			    <p>If we're all that disconnected from our bodies' signals, what should we do?</p>
+			  </div>
+	    </div>
+			<div class="carousel-item">
+	      <img class="d-block w-100" src="gallery/img/s4.png" alt="Third slide">
+				<div class="carousel-caption d-none d-md-block">
+			    <h3>Teach Yourself, Change The World</h3>
+			    <p>Education without the guidance of masters or institutions</p>
+			  </div>
+	    </div>
+			<div class="carousel-item">
+	      <img class="d-block w-100" src="gallery/img/s5.png" alt="Third slide">
+				<div class="carousel-caption d-none d-md-block">
+			    <h3>The 11 Factors Involved in Falling in Love, According to a Psychologist</h3>
+			    <p>Love is like a box of chocolates; there are a LOT of ingredients.</p>
+			  </div>
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouseSlideshow" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouseSlideshow" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+	<div class="container-fluid p-2">
+		<div class="row">
+			<div class="ly container col-md-2">
+				<ul class="p-3">
+					<li class="row">Amazing Places</li>
+					<li class="row">Brilliant Products</li>
+					<li class="row">Food & Culture</li>
+					<li class="row">Mind & Body</li>
+					<li class="row">Personal Growth</li>
+					<li class="row">Science & Technology</li>
+				</ul>
+			</div>
+			<div class="ly container col-md-10">
+				<div class="row">
+					<div class="col-md-8 lt-post">
+						<div class="card mb-2">
+							<img src="gallery/img/a9.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h4 class="card-title">This Is Your Brain On Love</h4></a>
+							   <p class="card-text">Find out how love and cocaine affect your brain in similar ways.</p>
+							 </div>
+						</div>
+						<div class="card mb-2">
+							<img src="gallery/img/a4.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h4 class="card-title">What Was the Future Supposed to be Like 100 Years Ago?</h4></a>
+						    <p class="card-text">Robot housekeepers? Check.</p>
+						  </div>
+						</div>
+						<div class="card mb-2">
+							<img src="gallery/img/a3.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h4 class="card-title">A Mystery of Einstein's Relativity Has Been Explained Using Classical Physics</h4></a>
+						    <p class="card-text">There's only one physics, people.</p>
+						  </div>
+						</div>
+						<div class="card mb-2">
+							<img src="gallery/img/a7.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h4 class="card-title">How Much Of Your Body Is Water? That All Depends.</h4></a>
+						    <p class="card-text">The old 70 percent rule isn't quite right.</p>
+						  </div>
+						</div>
+						<div class="card mb-2">
+							<img src="gallery/img/a1.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h4 class="card-title">Cuttlefish Prefer Cross-dressers</h4></a>
+						    <p class="card-text">Think of them as underwater David Bowies.</p>
+						  </div>
+						</div>
+					</div>
+
+					<div class="col-md-4 rt-post">
+						<div class="card mb-2">
+							<img src="gallery/img/a8.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h6 class="card-title">How the HyperX Cloud Alpha Solves Gaming Headsets' Muddy Bass Problem</h6></a>
+						  </div>
+						</div>
+						<div class="card mb-2">
+							<img src="gallery/img/a2.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h6 class="card-title">Scientists Make Stale Chips Taste Fresh With An Audio Edit</h6></a>
+						  </div>
+						</div>
+						<div class="card mb-2">
+							<img src="gallery/img/a5.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h6 class="card-title">Scientists Trimmed the Fat by Genetically Engineering Leaner Pigs</h6></a>
+						  </div>
+						</div>
+						<div class="card mb-2">
+							<img src="gallery/img/a0.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h6 class="card-title">Exercising Increases Your Self Control</h6></a>
+						  </div>
+						</div>
+						<div class="card mb-2">
+							<img src="gallery/img/a6.png" class="img-fluid rounded" />
+						  <div class="card-body p-2">
+						    <a href="#" class="card-link"><h6 class="card-title">When A Body Falls, The Necrobiome Rises</h6></a>
+						  </div>
+						</div>
+					</div>
+			</div>
+			</div>
+		</div>
+	</div>
+		<footer></footer>
+		<script src="gallery/js/jquery-3.2.1.slim.min.js">
+		</script><script src="gallery/js/popper.min.js"></script>
+		<script src="gallery/js/bootstrap.min.js"></script>
+	</body>
+	</html>
